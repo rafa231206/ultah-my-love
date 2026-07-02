c1140ac96765113802322ad959962ea860e7cafa
Raihan Fahrel Akbar <akbarberdikari@gmail.com>
2026-07-03 06:12:07 +0700

    Update start date and birthday message

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ typingObserver.observe(typing);
 // Tahun, Bulan-1, Tanggal
 // =========================
 
-const startDate = new Date(2026,3,18);
+const startDate = new Date(2025,6,15);
 
 function updateTime(){
 
