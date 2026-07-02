4c111c931aa647f0d1b6b3b5c7b62985266b49cf
Raihan Fahrel Akbar <akbarberdikari@gmail.com>
2026-07-03 04:31:39 +0700

    Update startDate to use August instead of July

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ typingObserver.observe(typing);
 // Tahun, Bulan-1, Tanggal
 // =========================
 
-const startDate = new Date(2026,07,09);
+const startDate = new Date(2026,08,09);
 
 function updateTime(){
 
